7dbb2feb822eeee80ac6b371e52c7dd03b062c0f
Jatin25k <87133160+Jatin25k@users.noreply.github.com>
2023-01-15 12:28:30 +0530

    Create script.js

---
+++ Day3 assignment/script.js
@@ -0,0 +1,14 @@
+//Program to check whether a number is even or odd
+let a=6
+function check(a){
+    if(a%2==0){
+        console.log(a + " is a even number.")
+    }
+    else{
+        console.log(a +" is a odd number." )
+    }
+}
+check(a)
+check(3)
+check(8)
+check(0)
